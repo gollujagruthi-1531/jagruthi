d4368e7e4c5020d0b65e195db84fc294288058ad
gollujagruthi-1531 <gollujagruthi@gmail.com>
2025-12-29 08:11:41 +0530

    added python program

---
+++ 1.py
@@ -0,0 +1,11 @@
+age = 18
+name = "jagruthi"
+height = 4.8
+are_you_student = True
+print("MY DETAILS")
+print(f"NAME: {name}")
+print(f"AGE: {age} years")
+print(f"HEIGHT:{height} feet")
+print(f"STUDENT:{are_you_student}")
+
+
