26ae8e3043ad8d93a1a103380a4b33353fa821f8
gollujagruthi-1531 <gollujagruthi@gmail.com>
2026-01-06 08:23:10 +0530

    added python program

---
+++ 6.py
@@ -0,0 +1,10 @@
+age = input("ENTER YOUR AGE: ")
+age = int(age)
+if age < 13:
+    print("YOUR ARE A CHILD")
+elif age <= 17:
+    print("YOUR ARE A TEENAGER")
+elif age<= 64:
+    print("YOU ARE AN ADULT")
+else:
+    print("YOU ARE A SENIOR")
