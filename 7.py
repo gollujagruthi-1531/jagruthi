49dc30476f8c453637bb6305d1b8298f524e451b
gollujagruthi-1531 <gollujagruthi@gmail.com>
2026-01-06 08:24:14 +0530

    added python program

---
+++ 7.py
@@ -0,0 +1,8 @@
+num = input("ENTER A NUMBER:")
+num = int(num)
+if num < 0:
+    print("THE NUMBER IS NEGATIVE")
+elif num > 0:
+    print("THE NUMBER IS POSITIVE")
+else:
+    print("THE NUMBER IS ZERO")
