b3be16b1432a0fbf76b5e71534d207073cc85db3
gollujagruthi-1531 <gollujagruthi@gmail.com>
2026-01-06 08:25:04 +0530

    added python program

---
+++ 8.py
@@ -0,0 +1,9 @@
+color = input("ENTER TRAFFIC LIGHT COLOUR(red/yellow/green):")
+if color == "red":
+    print("STOP!")
+elif color == "yellow":
+    print("PREPARE TO STOP")
+elif color == "green":
+    print("YOU CAN GO")  
+else:
+    print("WORNG INPUT!")
